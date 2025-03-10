de6f5ef1ee8ac9f66958c3762c29ec6a4883940f
DESKTOP-1CRRLJH\FernandoCBirznek <fernandocbirznek@hotmail.com>
2025-03-10 20:17:04 -0300

    teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  aulaApiUrl: 'http://ms-aula:80/api',
+  aulaApiUrl: 'http://ms-aula:8100/api',
   forumApiUrl: 'http://ms-forum:80/api',
   usuarioApiUrl: 'http://ms-usuario:80/api'
 };
