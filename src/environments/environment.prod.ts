cda9888a2165ea0264c20f59e23b23360c6e2173
DESKTOP-1CRRLJH\FernandoCBirznek <fernandocbirznek@hotmail.com>
2025-03-11 19:30:33 -0300

    novo teste docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  aulaApiUrl: 'http://ms-aula:8100/api',
-  forumApiUrl: 'http://ms-forum:80/api',
-  usuarioApiUrl: 'http://ms-usuario:80/api'
+  aulaApiUrl: 'http://ms-aula:5001/api',
+  forumApiUrl: 'http://ms-forum:5002/api',
+  usuarioApiUrl: 'http://ms-usuario:5000/api'
 };
